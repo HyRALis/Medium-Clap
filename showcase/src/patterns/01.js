540ea89b4867585bf8e2415dc77b79a4ca1eaaf4
HyRALis <petar.trajanoski.pt@gmail.com>
2022-03-23 22:26:21 +0100

    implemented the ref forwarding and HOC

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import withClapAnimation from '../hoc/withClapAnimation.js';
 import ClapCount from '../Subcomponents/ClapCount.js';
 import ClapIcon from '../Subcomponents/ClapIcon.js';
 import CountTotal from '../Subcomponents/CountTotal.js';
@@ -11,13 +12,14 @@ const initialState = {
     isClicked: false
 };
 
-export default function MediumClap() {
+export function MediumClap({ animationTimeline, clapRef }) {
     const MAXIMUM_INPUT_CLAP = 50;
 
     const [clapState, setClapState] = useState(initialState);
     const { count, countTotal, isClicked } = clapState;
 
     const handleClapClicked = () => {
+        animationTimeline.replay();
         setClapState((prevstate) => ({
             count: Math.min(prevstate.count + 1, MAXIMUM_INPUT_CLAP),
             countTotal: prevstate.count + 1 < MAXIMUM_INPUT_CLAP ? prevstate.countTotal + 1 : prevstate.countTotal,
@@ -26,10 +28,12 @@ export default function MediumClap() {
     };
 
     return (
-        <button className={styles.clap} onClick={handleClapClicked}>
+        <button ref={clapRef} id="clap" className={styles.clap} onClick={handleClapClicked}>
             <ClapIcon isClicked={isClicked} />
             <ClapCount count={count} />
             <CountTotal countTotal={countTotal} />
         </button>
     );
 }
+
+export default withClapAnimation(React.forwardRef((props, ref) => <MediumClap clapRef={ref} {...props} />));

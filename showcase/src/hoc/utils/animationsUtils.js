58f86d873634453b1833cd1a28f4a3fba7311287
HyRALis <petar.trajanoski.pt@gmail.com>
2022-03-23 22:23:34 +0100

    Implemented the HOC withClapAnimation and the animationUtils

---
+++ showcase/src/hoc/utils/animationsUtils.js
@@ -0,0 +1,79 @@
+import mojs from 'mo-js';
+
+export const getClapAnimations = (id) => {
+    const TIMELINE_DURATION = 300;
+    const COUNT_TOTAL_ANIMATION_DELAY = (3 * TIMELINE_DURATION) / 2;
+    const FADE_OUT_COUNT_ANIMATION_DELAY = TIMELINE_DURATION / 2;
+
+    const scaleButton = new mojs.Html({
+        el: `#${id}`,
+        duration: TIMELINE_DURATION,
+        scale: { 1.3: 1 },
+        easing: mojs.easing.ease.out
+    });
+
+    const countTotalAnimation = new mojs.Html({
+        el: `#clapCountTotal`,
+        duration: TIMELINE_DURATION,
+        delay: COUNT_TOTAL_ANIMATION_DELAY,
+        opacity: { 0: 1 },
+        y: { 0: -5 },
+        easing: mojs.easing.ease.out
+    });
+
+    const countAnimation = new mojs.Html({
+        el: `#clapCount`,
+        duration: TIMELINE_DURATION,
+        opacity: { 0: 1 },
+        y: { 0: -30 },
+        easing: mojs.easing.ease.out
+    }).then({
+        opacity: { 1: 0 },
+        y: -80,
+        delay: FADE_OUT_COUNT_ANIMATION_DELAY
+    });
+
+    const triangleBurstAnimation = new mojs.Burst({
+        parent: '#clap',
+        radius: { 50: 95 },
+        count: 5,
+        angle: 30,
+        children: {
+            shape: 'polygon',
+            radius: { 6: 0 },
+            stroke: 'rgba(211,54,0,0,0.5)',
+            strokeWidth: 2,
+            angle: 210,
+            delay: 30,
+            speed: 0.2,
+            easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+            duration: TIMELINE_DURATION
+        }
+    });
+
+    const circleBurstAnimation = new mojs.Burst({
+        parent: '#clap',
+        radius: { 50: 75 },
+        count: 5,
+        angle: 25,
+        children: {
+            shape: 'circle',
+            radius: { 3: 0 },
+            stroke: 'rgba(149,165,166,0,0.5)',
+            strokeWidth: 2,
+            angle: 210,
+            delay: 30,
+            speed: 0.2,
+            easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+            duration: TIMELINE_DURATION
+        }
+    });
+
+    return {
+        scaleButton,
+        countAnimation,
+        countTotalAnimation,
+        triangleBurstAnimation,
+        circleBurstAnimation
+    };
+};
---
+++ showcase/src/hoc/withClapAnimation.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import mojs from 'mo-js';
+
+import { getClapAnimations } from './utils/animationsUtils';
+
+export default function withClapAnimation(WrappedComponent) {
+    class WithClapAnimation extends Component {
+        animationTimeline = new mojs.Timeline();
+        state = {
+            animationTimeline: this.animationTimeline
+        };
+
+        componentDidMount() {
+            const { scaleButton, countAnimation, countTotalAnimation, triangleBurstAnimation, circleBurstAnimation } =
+                getClapAnimations(this.props.forwardedRef.current.id);
+            this.props.forwardedRef.current.style.transform = 'scale(1,1)';
+
+            this.setState({
+                animationTimeline: this.animationTimeline.add([
+                    scaleButton,
+                    countTotalAnimation,
+                    countAnimation,
+                    triangleBurstAnimation,
+                    circleBurstAnimation
+                ])
+            });
+        }
+
+        render() {
+            const { forwardedRef, ...rest } = this.props;
+
+            return <WrappedComponent {...rest} animationTimeline={this.state.animationTimeline} ref={forwardedRef} />;
+        }
+    }
+
+    return React.forwardRef((props, ref) => {
+        return <WithClapAnimation {...props} forwardedRef={ref} />;
+    });
+}
